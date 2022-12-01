716589b4cd8749e7d5b639205873b86897543a38
Golino98 <giacomogolino@gmail.com>
2022-12-01 18:55:12 +0100

    Funzionamento corretto della creazione di una soluzione iniziale feasible

--- src/InitialFeasibleSolution.java
+++ src/InitialFeasibleSolution.java
@@ -18,16 +18,14 @@ public class InitialFeasibleSolution {
     private int CalculateInitialFeasibleSolution() {
         int obj_fun = 0;
         int C_j;
-        for (int i = 0; i < s_l.size(); i++)
-        {
+        for (int i = 0; i < s_l.size(); i++) {
             C_j = 0;
-            for (int j = 0; j <= i; j++)
-            {
+            for (int j = 0; j <= i; j++) {
                 C_j = C_j + s_l.get(j).getProcessing_time();
             }
             C_j = C_j - s_l.get(i).getDue_date();
-            obj_fun = obj_fun + Integer.max(C_j,0);
+            obj_fun = obj_fun + Integer.max(C_j, 0);
         }
-        return  obj_fun;
+        return obj_fun;
     }
 }
